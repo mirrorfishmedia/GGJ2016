c5f691fff533d6fd97f2000d7642a587e17426aa
Matt Schell <matts@unity3d.com>
2016-01-29 22:09:42 -0500

    added navmesh, monk agent, back and forth behavior
    Signed-off-by: Matt Schell <matts@unity3d.com>

---
+++ Assets/MonkController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonkController : MonoBehaviour {
+
+	private NavMeshAgent agent;
+
+	public Transform destStack;
+	public Transform home;
+	public GameObject resource1;
+
+
+	private bool carryingResource = false;
+
+	// Use this for initialization
+	void OnEnable () 
+	{
+		agent = GetComponent<NavMeshAgent>();
+		agent.destination = destStack.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("ResourceStack")) 
+		{
+			carryingResource = true;
+			resource1.SetActive(true);
+			agent.destination = home.position;
+			transform.LookAt(agent.destination);
+		}
+
+		if (other.gameObject.CompareTag ("DropPoint")) 
+		{
+			carryingResource = false;
+			resource1.SetActive(false);
+			agent.destination = destStack.position;
+			transform.LookAt(agent.destination);
+		}
+
+
+	}
+}
